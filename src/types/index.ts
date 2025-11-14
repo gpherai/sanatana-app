922cf9254e2fa8556dafc214fba9a9cff007db2f
Gerald <g.pherai@proton.me>
2025-11-14 21:31:30 +0100

    Verwijder bestand

--- src/types/index.ts
+++
@@ -1,5 +0,0 @@
-// Central export for all types
-export * from './event'
-export * from './lunar'
-export * from './theme'
-export * from './api'
--- tailwind.config.ts
+++
@@ -1,3 +0,0 @@
-// Tailwind CSS v4 doesn't require a config file
-// Configuration is done via CSS using @theme directive in globals.css
-export default {}
